f60eef542cf68eb12e3f1152ae656e5f37709d10
Pittab <120305267+Pittab@users.noreply.github.com>
2026-06-01 19:42:30 +0100

    documentation (actually commenting the code) added and also made some small optimisations

--- editorcore.py
+++ editorcore.py
@@ -2,39 +2,41 @@
 #Copyright Eyescary development 2026
 #Modified? []
 
+#imports
 import extensions
 import os
 
+#put your imports here
+
 class editor:
-    def openfile(filename):
+    def openfile(self): #copies the file contents to self.lines
         try:
-            with open(filename, "r") as file:
+            with open(self.filename, "r") as file:
                 return file.readlines()
         except:
             return []
         
-    def write(self, filename):
-        with open(filename, "w") as file:
-            for item in self.lines:
-                file.write(item)
+    def write(self): #writes self.lines to the file
+        with open(self.filename, "w") as file:
+            file.write(''.join(self.lines))
 
-    def printfilecore(self):
+    def printfilecore(self): #basic print file function
         os.system("cls" if os.name == "nt" else "clear")
         linenum = 1
         for item in self.lines:
             print(str(linenum)+"|"+item, end="")
             linenum+=1
 
-    def main(self):
+    def main(self): #main editor function
         editor.printfilecore(self)
         userInput=input("|")
         if userInput.startswith(":"):
             self.lines = extensions.commands(userInput.strip(), self.lines, self.filename, self.extension)
         else:
             self.lines.append(userInput+'\n')
-        editor.write(self, self.filename)
+        editor.write(self)
 
-    def __init__(self):
+    def __init__(self): #init menu
         name=input("what is the name of the file you wish to edit?: ")
         if "." in name:
             self.extension = "." + name.split(".")[1]
@@ -44,10 +46,12 @@ class editor:
             if not self.extension.startswith("."):
                 self.extension = "." + self.extension
             self.filename = name + self.extension
-        print(self.filename, self.extension)
-        self.lines = editor.openfile(self.filename)
+        self.lines = editor.openfile(self)
 
+def main():
+    instance = editor()
+    while True:
+        instance.main()
 
-instance = editor()
-while True:
-    instance.main()
+if __name__ == "__main__":
+    main()
